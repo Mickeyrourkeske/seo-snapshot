fe631328ccc89c60fb2ca1f62876c5c4bba3ad49
Mickeyrourkeske <mickeyrourkeske@0011011.org>
2014-12-04 13:40:04 +0100

    recursive iterate through all pages

--- src/Snapshot.js
+++ src/Snapshot.js
@@ -3,20 +3,145 @@
  */
 
 var phantom = require('phantom');
-var env = require('jsdom').env;
+var url = require('url');
 var fs = require('fs');
+var path = require('path');
+var mkpath = require('mkpath');
 
+
+var discardLink = /(\.pdf)|(feed)|(\.jpg)|(\.jpeg)|(\.png)/gi;
 
 function Snapshot(uri, snapshotFolder) {
     this.uri = uri;
+
+    if (!snapshotFolder.indexOf('/', 0)) {
+        snapshotFolder = __dirname + '/' + snapshotFolder;
+    }
     this.snapshotFolder = snapshotFolder;
+
+    this.linkQueue = {};
 }
 
 
 Snapshot.prototype.generate = function (cb) {
 
+    console.log('Get page: ' + this.uri);
+    this.recursiveQueue(this.uri, cb);
 
-    generateHtmlAndGetLinks(this.uri, cb);
+};
+
+Snapshot.prototype.recursiveQueue = function (uri, cb) {
+
+    this.generateSinglePage(uri, function (err, result) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            /* add new links and increment them */
+            for (var i = 0; i < result.links.length; i++) {
+                if (!this.linkQueue.hasOwnProperty(result.links[i])) {
+                    this.linkQueue[result.links[i]] = {
+                        count: 0,
+                        visit: false
+                    };
+                    console.log('Add new link: ' + result.links[i]);
+                }
+                this.linkQueue[result.links[i]].count++;
+            }
+            /* if the current location is new */
+            if (!this.linkQueue.hasOwnProperty(result.location)) {
+                this.linkQueue[result.location] = {
+                    count: 0
+                }
+            }
+            /* we visited the current location */
+            this.linkQueue[result.location].visit = true;
+
+            /* check if we got redirected to result.location */
+            if (result.location !== result.origin) {
+                /* add origin to location */
+                if (!this.linkQueue[result.location].origins) {
+                    this.linkQueue[result.location].origins = [];
+                }
+                this.linkQueue[result.location].origins.push(result.origin);
+
+                /* check if the origin location is new */
+                if (!this.linkQueue.hasOwnProperty(result.origin)) {
+                    this.linkQueue[result.origin] = {
+                        count: 0
+                    }
+                }
+                /* we somehow visited the origin site */
+                this.linkQueue[result.origin].visit = true;
+                /* add redirect to origin location */
+                this.linkQueue[result.origin].redirect = result.location;
+            }
+
+
+            console.log(JSON.stringify(this.linkQueue, null, 2));
+
+            var breaked = false;
+            for (var link in this.linkQueue) {
+                if (this.linkQueue.hasOwnProperty(link)) {
+                    if (!this.linkQueue[link].visit) {
+                        if (!link.match(discardLink)) {
+                            console.log('Get page: ' + link);
+                            this.recursiveQueue(link, cb);
+                            breaked = true;
+                            break;
+                        }
+                        else {
+                            this.linkQueue[link].visit = true;
+                            this.linkQueue[link].discard = true;
+                        }
+                    }
+                }
+            }
+            if (!breaked) {
+                cb();
+            }
+        }
+    }.bind(this));
+};
+
+
+Snapshot.prototype.generateSinglePage = function (uri, cb) {
+    generateHtmlAndGetLinks(uri, function (err, result) {
+        if (err) {
+            //TODO
+            console.log(err);
+            cb(err);
+        }
+
+        var file = this.snapshotFolder + url.parse(result.location.replace('/#!', '')).path;
+        if (file.indexOf('/', file.length - 1) !== -1) { // path ends with '/'
+            file += 'index.html';
+        }
+        else {
+            file += '.html';
+        }
+
+        mkpath(path.dirname(file), function (err) {
+            if (err) {
+                //TODO
+                console.log(err);
+            }
+            else {
+                fs.writeFile(file, result.content, function (err) {
+                    if (err) {
+                        //TODO
+                        console.log(err);
+                    }
+                    else {
+                        console.log('Saved ' + file);
+                    }
+                });
+            }
+        });
+
+
+        cb(undefined, result);
+    }.bind(this));
 };
 
 
@@ -28,7 +153,10 @@ function generateHtmlAndGetLinks(uri, cb) {
                 height: 800
             });
             page.open(uri, function (status) {
-                console.log('Opened ' + uri + ' ' + status);
+                if (status !== 'success') {
+                    cb(new Error('Opened ' + uri + ' ' + status));
+                    return;
+                }
 
                 page.injectJs('jquery', function () {
 
@@ -39,10 +167,10 @@ function generateHtmlAndGetLinks(uri, cb) {
                             $('meta[name=fragment]').remove();
 
                             var links = [];
+                            var location = window.location.href;
                             $('a').each(function () {
                                 links.push(this.href);
                             });
-                            var location = window.location.href;
                             var content = $('html')[0].outerHTML;
 
                             return {
@@ -51,17 +179,28 @@ function generateHtmlAndGetLinks(uri, cb) {
                                 content: content
                             };
                         }, function (result) {
-
-                            console.log(result.location);
-                            console.log(result.links);
-                            fs.writeFile('tmp', result.content, function (err) {
-                                cb(err);
-                            });
-
                             ph.exit();
+
+                            try {
+                                // remove links which point to somewhere else
+                                var links = [];
+                                for (var i = 0; i < result.links.length; i++) {
+                                    if (url.parse(result.location).hostname === url.parse(result.links[i]).hostname) {
+                                        links.push(result.links[i]);
+                                    }
+                                }
+
+                                result.links = links;
+                                result.origin = uri;
+
+                                cb(undefined, result);
+                            }
+                            catch (err) {
+                                cb(err);
+                            }
                         });
                     }, 5000);
-                })
+                });
             });
         });
     });
